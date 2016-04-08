75c5fae2d2e2d2a2d3602ef1a3823efed524d935
psy888 <lpsyl888l@yandex.ru>
2016-04-08 15:52:15 +0300

    add js to form and animate

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ $(document).ready(function(){
 		$(this).text('Liked!').closest('.news-item').addClass('is-liked');
 
 	});
+
+	$('.js-add-link').on('click', function(event){
+		event.preventDefault();
+
+		$('.js-form').toggleClass('is-visible');
+	});
 });
