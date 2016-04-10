ea097323e82b33e933dc008019e22638fb9ee020
psy888 <lpsyl888l@yandex.ru>
2016-04-10 08:11:14 +0300

    Add JS to modal

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
+
+$(document).ready(function(){
+
 //------------------------------------------
 //			DOCUMENT READY
 //------------------------------------------
 
-$(document).ready(function(){
-	$('.js-like').on('click', function(event){
+
+	$('.js-like').on('click', function(event){  //Like ANCHOR
 		event.preventDefault();
 
 		$(this).text('Liked!').closest('.news-item').addClass('is-liked');
 
 	});
 
-	$('.js-add-link').on('click', function(event){
+	$('.js-add-link').on('click', function(event){  //ADD LINK ANCHor
 		event.preventDefault();
 
 		$('.js-form').toggleClass('is-visible');
 	});
+
+	$('.js-show-modal').on('click', function(e){      ///SHOW MODAL
+		e.preventDefault();  
+
+		$('.modal').addClass('is-visible');
+		$('.js-modal-overlay').addClass('is-visible');
+	});
+
+	$('.js-modal-overlay').on('click', function(e){    ///HIDE MODAL
+		e.preventDefault();
+
+		$('.modal').removeClass('is-visible');
+		$('.js-modal-overlay').removeClass('is-visible');
+	});
+
+
 });
